251e7858e7a3c01638d655cd5467462b73427f52
否泰 <67478731+Cai-yizhe@users.noreply.github.com>
2023-10-13 22:41:43 +0800

    Add files via upload

---
+++ first.py
@@ -0,0 +1,7 @@
+name = input("name:")
+Chinese = float(input("Chinese scores:"))
+Math = float(input("Math scores:"))
+English = float(input("English scores:"))
+sum = Chinese + Math + English
+print("总分", sum)
+print("平均", sum / 3)
